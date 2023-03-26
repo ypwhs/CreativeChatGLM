7e5588982a4026e98e8e77e612bfea6c07762603
ypw <yangpeiwen@gzsonic.com>
2023-03-26 20:46:41 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -75,10 +75,10 @@ with gr.Blocks(css=""".message {
         """
 # 💡Creative ChatGLM WebUI
 
-👋 欢迎来到 ChatGLM 创意世界！
+👋 欢迎来到 ChatGLM 创意世界！[https://github.com/ypwhs/CreativeChatGLM](https://github.com/ypwhs/CreativeChatGLM)
 
-* 📝 你可以使用“修订”按钮修改最后一句 ChatGLM 的回复。
 * 📖 你可以使用“续写”按钮帮 ChatGLM 想一个开头，并让它继续生成更多的内容。
+* 📝 你可以使用“修订”按钮修改最后一句 ChatGLM 的回复。
 """)
     with gr.Row():
         with gr.Column(scale=4):
