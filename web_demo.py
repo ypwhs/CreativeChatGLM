20aecae74c5ddbda581d0a89d98a0d1ef2345001
ypw <yangpeiwen@gzsonic.com>
2023-03-26 18:35:41 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -1,48 +1,67 @@
-from chatglm.modeling_chatglm import ChatGLMForConditionalGeneration
-from chatglm.tokenization_chatglm import ChatGLMTokenizer
 import gradio as gr
 
-tokenizer = ChatGLMTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True, resume_download=True)
-model = ChatGLMForConditionalGeneration.from_pretrained(
-    "THUDM/chatglm-6b", trust_remote_code=True, resume_download=True).half().cuda()
+debug = False
 
-model = model.eval()
+if debug:
+
+    def load_model():
+        pass
+
+    def inference(input, max_length, top_p, temperature, history=None):
+        import random
+        sample_outputs = [
+            '我是杨开心。',
+            '我两岁半了。',
+            '我喜欢吃雪糕。',
+        ]
+        one_output = random.choice(sample_outputs)
+        for i in range(len(one_output)):
+            yield one_output[:i + 1]
+else:
+    from chatglm.modeling_chatglm import ChatGLMForConditionalGeneration
+    from chatglm.tokenization_chatglm import ChatGLMTokenizer
+    tokenizer = ChatGLMTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True, resume_download=True)
+    model = ChatGLMForConditionalGeneration.from_pretrained(
+        "THUDM/chatglm-6b", trust_remote_code=True, resume_download=True).half().cuda()
+
+    model = model.eval()
+
+    def inference(input, max_length, top_p, temperature, history=None):
+        if history is None:
+            history = []
+        for response, history in model.stream_chat(tokenizer, input, history, max_length=max_length, top_p=top_p,
+                                                   temperature=temperature):
+            yield response
+
+
+def predict(input, max_length, top_p, temperature, history):
+    if history is None:
+        history = []
+    history.append((input, ""))
+    for response in inference(input, max_length, top_p, temperature, history):
+        history[-1] = (history[-1][0], response)
+        yield history, ''
+
 
 MAX_TURNS = 20
 MAX_BOXES = MAX_TURNS * 2
 
-
-def predict(input, max_length, top_p, temperature, history=None):
-    if history is None:
-        history = []
-    for response, history in model.stream_chat(tokenizer, input, history, max_length=max_length, top_p=top_p,
-                                               temperature=temperature):
-        updates = []
-        for query, response in history:
-            updates.append(gr.update(visible=True, value="用户：" + query))
-            updates.append(gr.update(visible=True, value="ChatGLM-6B：" + response))
-        if len(updates) < MAX_BOXES:
-            updates = updates + [gr.Textbox.update(visible=False)] * (MAX_BOXES - len(updates))
-        yield [history] + updates
-
-
-with gr.Blocks() as demo:
-    state = gr.State([])
-    text_boxes = []
-    for i in range(MAX_BOXES):
-        if i % 2 == 0:
-            text_boxes.append(gr.Markdown(visible=False, label="提问："))
-        else:
-            text_boxes.append(gr.Markdown(visible=False, label="回复："))
-
+with gr.Blocks(css=""".message {
+    width: inherit !important;
+    padding-left: 20px !important;
+}""") as demo:
     with gr.Row():
         with gr.Column(scale=4):
-            txt = gr.Textbox(
-                show_label=False, placeholder="Enter text and press enter", lines=11).style(container=False)
+            chatbot = gr.Chatbot(elem_id="chat-box", show_label=False).style(height=800)
         with gr.Column(scale=1):
-            max_length = gr.Slider(32, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
-            top_p = gr.Slider(0.01, 1, value=0.7, step=0.01, label="Top P", interactive=True)
-            temperature = gr.Slider(0.01, 5, value=0.95, step=0.01, label="Temperature", interactive=True)
-            button = gr.Button("Generate")
-    button.click(predict, [txt, max_length, top_p, temperature, state], [state] + text_boxes)
-demo.queue().launch(server_name='0.0.0.0', share=False, inbrowser=False)
+            with gr.Row():
+                max_length = gr.Slider(32, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
+                top_p = gr.Slider(0.01, 1, value=0.7, step=0.01, label="Top P", interactive=True)
+                temperature = gr.Slider(0.01, 5, value=0.95, step=0.01, label="Temperature", interactive=True)
+            with gr.Row():
+                txt = gr.Textbox(
+                    show_label=False, placeholder="Enter text and press enter", lines=4).style(container=False)
+                generate_button = gr.Button("Generate")
+    history = gr.State([])
+    generate_button.click(predict, inputs=[txt, max_length, top_p, temperature, history], outputs=[chatbot, txt])
+demo.queue().launch(server_name='0.0.0.0', server_port=7860, share=False, inbrowser=False)
