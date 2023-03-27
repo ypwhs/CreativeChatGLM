7293dda3806cfaa78e1453cc9c4bf0b48dcf5ca6
ypw <915505626@qq.com>
2023-03-27 15:05:29 +0800

    Update offline_environment.bat

--- offline_environment.bat
+++ offline_environment.bat
@@ -7,4 +7,3 @@ set PY_LIBS=%DIR%\python\Scripts\Lib;%DIR%\python\Scripts\Lib\site-packages
 set PY_PIP=%DIR%\python\Scripts
 set SKIP_VENV=1
 set PIP_INSTALLER_LOCATION=%DIR%\python\get-pip.py
-set TRANSFORMERS_CACHE=%DIR%\transformers-cache
