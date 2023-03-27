8dfb29ee100c6cff1c5a234c885cf754b79c4464
ypw <915505626@qq.com>
2023-03-27 13:29:28 +0800

    Create offline_environment.bat

---
+++ offline_environment.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set DIR=%~dp0system
+
+set PATH=C:\Windows\system32;C:\Windows;%DIR%\git\bin;%DIR%\python;%DIR%\python\Scripts;%DIR%\python\Lib\site-packages\torch\lib
+set PY_LIBS=%DIR%\python\Scripts\Lib;%DIR%\python\Scripts\Lib\site-packages
+set PY_PIP=%DIR%\python\Scripts
+set SKIP_VENV=1
+set PIP_INSTALLER_LOCATION=%DIR%\python\get-pip.py
+set TRANSFORMERS_CACHE=%DIR%\transformers-cache
