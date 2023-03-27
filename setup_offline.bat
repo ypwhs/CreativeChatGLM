59d17a281af1486fd9631faef58a5de05ce75b91
ypw <915505626@qq.com>
2023-03-27 13:09:45 +0800

    Update setup_offline.bat

--- setup_offline.bat
+++ setup_offline.bat
@@ -1,12 +1,12 @@
 cd /D "%~dp0"
 
 echo "Setup VENV"
-call environment.bat
+call ../environment.bat
 
 python %PIP_INSTALLER_LOCATION%
 
-echo "Install dependencies"
+echo "Install dependencies..."
 pip install torch==2.0.0+cu118 --index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
-pip install --upgrade -r CreativeChatGLM/requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
-
+pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+echo "Install finished."
 pause
