25d74007070c7621f4ac4b2e85bba79f4098dc5e
ypw <yangpeiwen@gzsonic.com>
2023-03-27 22:56:10 +0800

    Update start_venv.bat

--- start_venv.bat
+++ start_venv.bat
@@ -1,17 +1,8 @@
 @echo off
 cd /D "%~dp0"
-if exist .venv goto :start
 
-echo Setup venv
-call setup_env.bat
-goto :run
+call env_venv.bat
 
-:start
-echo Start venv
-call .venv\Scripts\activate.bat
-goto :run
-
-:run
 echo Start WebUI
 python web_demo.py
 pause
