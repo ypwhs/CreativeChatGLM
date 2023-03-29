f4891bbb41d8dd5b5afa96909bd02df6b1cc58af
ypw <yangpeiwen@gzsonic.com>
2023-03-29 22:55:20 +0800

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -2,16 +2,18 @@ from glob import glob
 from huggingface_hub import snapshot_download
 
 model_name_list = [
-    'THUDM/chatglm-6b',
-    'THUDM/chatglm-6b-int4',
     'THUDM/chatglm-6b-int4-qe',
-    'THUDM/glm-10b-chinese',
-    'BelleGroup/BELLE-7B-2M',
-    'BelleGroup/BELLE-7B-gptq',
-    'BelleGroup/BELLE-LLAMA-7B-2M',
-    'BelleGroup/BELLE-LLAMA-7B-2M-gptq',
-    'silver/chatglm-6b-slim',
+    'THUDM/chatglm-6b-int4',
+
     'silver/chatglm-6b-int4-qe-slim',
+    'BelleGroup/BELLE-LLAMA-7B-2M-gptq',
+    'BelleGroup/BELLE-7B-gptq',
+
+    'THUDM/chatglm-6b',
+    'THUDM/glm-10b-chinese',
+    'BelleGroup/BELLE-LLAMA-7B-2M',
+    'BelleGroup/BELLE-7B-2M',
+    'silver/chatglm-6b-slim',
 ]
 
 for model_name in model_name_list:
