4b24f834f95e52f99c34af32bf235356c895681f
ypw <915505626@qq.com>
2023-03-29 11:28:13 +0800

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,7 +1,20 @@
+from glob import glob
 from huggingface_hub import snapshot_download
 
-snapshot_download(
-    repo_id='THUDM/chatglm-6b-int4',
-    local_dir='THUDM/chatglm-6b-int4',
-    local_dir_use_symlinks=False
-)
+model_name_list = [
+    'THUDM/chatglm-6b',
+    'THUDM/chatglm-6b-int4',
+    'THUDM/chatglm-6b-int4-qe',
+    'THUDM/glm-10b-chinese',
+]
+
+for model_name in model_name_list:
+    if glob(f'{model_name}/*.bin'):
+        print(f'{model_name} already downloaded')
+        continue
+    print(f'Downloading {model_name}')
+    snapshot_download(
+        repo_id=model_name,
+        local_dir=model_name,
+        local_dir_use_symlinks=False)
+    print(f'{model_name} downloaded')
