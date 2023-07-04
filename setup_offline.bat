f051f2b62d04614f9f5460578a6c058d628796ad
杨培文 (Yang Peiwen) <915505626@qq.com>
2023-07-04 09:14:32 +0800

    1

--- setup_offline.bat
+++ setup_offline.bat
@@ -11,7 +11,7 @@ python %PIP_INSTALLER_LOCATION%
 :install_python_packages
 echo Install dependencies...
 pip install torch==2.0.0+cu118 --index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
-pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+pip install -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 
 echo Install finished.
 pause
--- setup_venv.bat
+++ setup_venv.bat
@@ -5,7 +5,7 @@ call env_venv.bat
 
 echo Install dependencies...
 pip install torch==2.0.0+cu118 --index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
-pip install --upgrade -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+pip install -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 
 echo Install finished.
 pause
