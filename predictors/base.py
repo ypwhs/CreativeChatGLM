157e3548b8b1f3b52576f0a85e14504049a10d09
ypw <915505626@qq.com>
2023-12-10 15:39:23 +0800

    Update base.py

--- predictors/base.py
+++ predictors/base.py
@@ -12,7 +12,8 @@ def parse_codeblock(text):
                 lines[i] = '</code></pre>'
         else:
             if i > 0:
-                lines[i] = "<br/>" + line.replace("<", "&lt;").replace(">", "&gt;")
+                lines[i] = "<br/>" + line.replace("<", "&lt;").replace(
+                    ">", "&gt;")
     return "".join(lines)
 
 
@@ -28,8 +29,8 @@ class BasePredictor(ABC):
         raise NotImplementedError
 
     def predict_continue(self, query, latest_message, max_length, top_p,
-                         temperature, allow_generate, history, last_state, *args,
-                         **kwargs):
+                         temperature, allow_generate, history, last_state,
+                         *args, **kwargs):
         last_state[0] = copy.deepcopy(history)
         last_state[1] = query
         last_state[2] = latest_message
@@ -49,6 +50,35 @@ class BasePredictor(ABC):
             history_colorful = copy.deepcopy(history)
             colorful_response = f'<span style="color:red">{latest_message}</span>{response[len(latest_message):]}'
             history_colorful[-1] = (history_colorful[-1][0], colorful_response)
+            yield history_colorful, '', ''
+            if not allow_generate[0]:
+                break
+
+    def predict_continue_dict(self, query, latest_message, max_length, top_p,
+                              temperature, allow_generate, history, last_state,
+                              *args, **kwargs):
+        last_state[0] = copy.deepcopy(history)
+        last_state[1] = query
+        last_state[2] = latest_message
+        if history is None:
+            history = []
+        allow_generate[0] = True
+        history.append({"role": "user", "content": query})
+        history.append({"role": "assistant", "content": latest_message})
+        for response in self.stream_chat_continue(
+                self.model,
+                self.tokenizer,
+                query=query,
+                history=history,
+                max_length=max_length,
+                top_p=top_p,
+                temperature=temperature):
+            # 更新最后一条历史记录的回答
+            history[-1]["content"] = response
+            history_colorful = copy.deepcopy(history)
+            # 创建一个带有颜色的响应，这里的逻辑可能需要根据实际情况进行调整
+            colorful_response = f'<span style="color:red">{latest_message}</span>{response[len(latest_message):]}'
+            history_colorful[-1]["content"] = colorful_response
             yield history_colorful, '', ''
             if not allow_generate[0]:
                 break
