ef8729567298662763d9da81126dad2bb8991da9
ypw <915505626@qq.com>
2023-12-10 14:59:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -79,21 +79,21 @@ with gr.Blocks(css=""".message {
 """)
     with gr.Row():
         with gr.Column(scale=4):
-            chatbot = gr.Chatbot(elem_id="chat-box", show_label=False).style(height=850)
+            chatbot = gr.Chatbot(elem_id="chat-box", show_label=False, height=850)
         with gr.Column(scale=1):
             with gr.Row():
                 max_length = gr.Slider(32, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
                 top_p = gr.Slider(0.01, 1, value=0.7, step=0.01, label="Top P", interactive=True)
                 temperature = gr.Slider(0.01, 5, value=0.95, step=0.01, label="Temperature", interactive=True)
             with gr.Row():
-                query = gr.Textbox(show_label=False, placeholder="Prompts", lines=4).style(container=False)
+                query = gr.Textbox(show_label=False, placeholder="Prompts", lines=4)
                 generate_button = gr.Button("生成")
             with gr.Row():
                 continue_message = gr.Textbox(
-                    show_label=False, placeholder="Continue message", lines=2).style(container=False)
+                    show_label=False, placeholder="Continue message", lines=2)
                 continue_btn = gr.Button("续写")
                 revise_message = gr.Textbox(
-                    show_label=False, placeholder="Revise message", lines=2).style(container=False)
+                    show_label=False, placeholder="Revise message", lines=2)
                 revise_btn = gr.Button("修订")
                 revoke_btn = gr.Button("撤回")
                 regenerate_btn = gr.Button("重新生成")
@@ -117,5 +117,5 @@ with gr.Blocks(css=""".message {
                          outputs=[chatbot, query, continue_message])
     interrupt_btn.click(interrupt, inputs=[allow_generate])
 
-demo.queue(concurrency_count=4).launch(server_name='0.0.0.0', server_port=7860, share=False, inbrowser=False)
+demo.queue().launch(server_name='0.0.0.0', server_port=7860, share=False, inbrowser=False)
 demo.close()
