c327716387dfbea479a0e30ddffe8443b260bc19
ypw <915505626@qq.com>
2024-04-07 15:37:01 +0800

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -7,30 +7,19 @@ from huggingface_hub import snapshot_download
 
 model_name_list = [
     # 'THUDM/chatglm-6b-int4-qe',
-    'THUDM/chatglm-6b-int4',
-    'THUDM/chatglm-6b',
+    # 'THUDM/chatglm-6b-int4',
+    # 'THUDM/chatglm-6b',
     # 'THUDM/glm-10b-chinese',
 
-    'THUDM/chatglm2-6b',
+    # 'THUDM/chatglm2-6b',
     'THUDM/chatglm2-6b-int4',
 
     'THUDM/chatglm3-6b',
     'THUDM/chatglm3-6b-128k',
 
-    # 'BelleGroup/BELLE-LLAMA-7B-2M-gptq',
-    # 'BelleGroup/BELLE-7B-gptq',
-    # 'BelleGroup/BELLE-LLAMA-7B-2M',
-    # 'BelleGroup/BELLE-7B-2M',
-
     # 'silver/chatglm-6b-slim',
     # 'silver/chatglm-6b-int4-slim',
     # 'silver/chatglm-6b-int4-qe-slim',
-
-    # 'fnlp/moss-moon-003-base',
-    # 'fnlp/moss-moon-003-sft',
-    # 'fnlp/moss-moon-003-sft-plugin',
-    # 'fnlp/moss-moon-003-sft-int4',
-    # 'fnlp/moss-moon-003-sft-plugin-int4'
 ]
 
 for model_name in model_name_list:
