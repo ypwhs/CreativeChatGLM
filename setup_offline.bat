e59db2ef15daa68b9d2e4a9bff93d1b69dea8f61
ypw <915505626@qq.com>
2024-07-07 13:43:02 +0800

    Update setup_offline.bat

--- setup_offline.bat
+++ setup_offline.bat
@@ -1,5 +1,7 @@
 cd /D "%~dp0"
 
+rem set http_proxy=http://127.0.0.1:7890 & set https_proxy=http://127.0.0.1:7890
+
 echo Setup offline environment
 call env_offline.bat
 
@@ -10,7 +12,7 @@ python %PIP_INSTALLER_LOCATION%
 
 :install_python_packages
 echo Install dependencies...
-pip install torch==2.0.0+cu118 --index-url https://download.pytorch.org/whl/cu118 --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
+pip install torch==2.3.1 torchvision==0.18.1 --index-url https://download.pytorch.org/whl/cu121 --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
 pip install -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
 
 echo Install finished.
